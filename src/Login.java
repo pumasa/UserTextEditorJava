71b9e983635d72eeed95746a583e2af8696cfc1c
Mike <misha9584@gmail.com>
2022-07-19 12:43:19 -0700

    Updated login class so it can use stored data to login with correct user and password

--- src/Login.java
+++ src/Login.java
@@ -2,6 +2,13 @@ import java.awt.CardLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.StringTokenizer;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -51,8 +58,49 @@ public class Login extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		add(new Register(), "register");
-		cl.show(this, "register");
+		
+		if(e.getSource() == login) {
+			try {
+				BufferedReader input = new BufferedReader(new FileReader("passwords.txt"));
+				String pass = null;
+				String line = input.readLine();
+				while(line != null) {
+					StringTokenizer st = new StringTokenizer(line);
+					if(userTF.getText().equals(st.nextToken()))
+						pass = st.nextToken();
+					line = input.readLine();
+				}
+				
+				input.close();
+				
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+				md.update(new String(passTF.getPassword()).getBytes());
+				byte byteData[] = md.digest();
+				StringBuffer sb = new StringBuffer();
+				for (int i=0; i< byteData.length; i++)
+					sb.append(Integer.toString((byteData[i] & 0xFF) + 0x100, 16).substring(1));
+				
+				if (pass.equals(sb.toString()))
+					System.out.println("You have logged in");
+				
+				
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (NoSuchAlgorithmException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		if(e.getSource() == register) {
+			add(new Register(), "register");
+			cl.show(this, "register");
+		}
+		
 		
 		
 	}
